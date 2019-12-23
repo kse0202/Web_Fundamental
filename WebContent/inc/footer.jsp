944589916835619081279ed37184449e400ef415
kse0202 <kse0202@naver.com>
2019-12-23 18:37:54 +0900

    2019-12-23-v1

---
+++ WebContent/inc/footer.jsp
@@ -0,0 +1,21 @@
+<%@ page pageEncoding="utf-8"%>
+
+  <!-- footer start-->
+  <footer class="bg-dark bd-footer" style="color:white;padding:1em;margin-top: 1em;">
+    <div class="container-fluid text-center">
+      <p>Since 2019-12-13</p>
+    </div>
+  </footer>
+  <!-- footer end-->
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="../js/jquery-3.4.1.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+    integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+    integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ src/kr/co/acorn/dto/MemberDto.java
@@ -0,0 +1,55 @@
+package kr.co.acorn.dto;
+
+public class MemberDto {
+	private String email;
+	private String name;
+	private String password;
+	private String phone;
+	private String regdate;
+	
+	
+	public MemberDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public MemberDto(String email, String name, String password, String phone, String regdate) {
+		super();
+		this.email = email;
+		this.name = name;
+		this.password = password;
+		this.phone = phone;
+		this.regdate = regdate;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(String regdate) {
+		this.regdate = regdate;
+	}
+
+}
